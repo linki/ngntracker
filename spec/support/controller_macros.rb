0f2cda67ce7a1b53864cbe6d9d5a8488c6947102
Martin Linkhorst <martin.linkhorst@student.hpi.uni-potsdam.de>
2009-10-11 13:29:13 +1000

    added rails footnotes, finxed authlogic specs

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,9 +1,10 @@
 module ControllerMacros
   def login_as_user
-    login!(User.find_by_username('martin') || Factory(:user, :username => 'martin', :password => 'secret')) 
+    login(User.find_by_username('martin') || Factory(:user, :username => 'martin', :password => 'secret')) 
   end
 
-  def login!(user)
+  def login(user)
+    activate_authlogic
     UserSession.stubs(:find).returns(UserSession.create!(user))
   end  
 end
