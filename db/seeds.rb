5b4aee4b317f5edfa1fbd938be949a5d038b781f
Martin Linkhorst <martin.linkhorst@student.hpi.uni-potsdam.de>
2009-10-12 19:54:17 +1000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+['Messe Berlin', 'NürnbergMesse', 'Hannover Messe'].each do |name|
+  Organizer.find_or_create_by_name(name)
+end
+
+['ITB', 'IFA'].each do |name|
+  portal = Portal.find_or_initialize_by_name(name)
+  portal.organizer = Organizer.find_by_name!('Messe Berlin')
+  portal.save!
+end
+
+['Fachpack', 'Biofach'].each do |name|
+  portal = Portal.find_or_initialize_by_name(name)
+  portal.organizer = Organizer.find_by_name!('NürnbergMesse')
+  portal.save!  
+end
+
+['Cebit'].each do |name|
+  portal = Portal.find_or_initialize_by_name(name)
+  portal.organizer = Organizer.find_by_name!('Hannover Messe')
+  portal.save!  
+end
+
+['Wording', 'Frontend', 'Backend'].each do |name|
+  Category.find_or_create_by_name(name)
+end
+
+['Spelling', 'Inconsistency'].each do |name|
+  category = Category.find_or_initialize_by_name(name)
+  category.parent = Category.find_by_name!('Wording')
+  category.save! 
+end
+
+['Redsys'].each do |name|
+  category = Category.find_or_initialize_by_name(name)
+  category.parent = Category.find_by_name!('Backend')
+  category.save! 
+end
